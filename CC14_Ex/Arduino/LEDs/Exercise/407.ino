74e8846a1db82a82af01cf8f5421ed0648cbaf2a
grassnhi <nhi.nguyenn@hcmut.edu.vn>
2021-12-09 18:55:50 +0700

    Edit 407

--- CC14_Ex/Arduino/LEDs/Exercise/407.ino
+++ CC14_Ex/Arduino/LEDs/Exercise/407.ino
@@ -11,26 +11,75 @@ void setup(){
 
 void displayNumber(int i){
   if (i == 0){
-    digitalWrite(0, HIGH);
+   	digitalWrite(4, HIGH);
     
+    digitalWrite(1, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(0, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    
+  }else if(i == 1){
+  	digitalWrite(0, LOW);
     digitalWrite(1, LOW);
     digitalWrite(2, LOW);
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
     digitalWrite(5, LOW);
+    digitalWrite(6, LOW);	 
+
+  }else if(i == 2){
+    digitalWrite(2, LOW);    
+    digitalWrite(3, LOW);
     digitalWrite(6, LOW);
-    
-  }else if(i == 1){
-  	 digitalWrite(0, HIGH);
-    
+
+    digitalWrite(0, HIGH);
     digitalWrite(1, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+
+  }else if(i == 3){
+    digitalWrite(3, HIGH);
+    
+    digitalWrite(1, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(0, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+
+  }else if(i == 4){
+    digitalWrite(3, HIGH);    
+    digitalWrite(4, HIGH);
+    
+    digitalWrite(0, LOW);
+    digitalWrite(1, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+  
+  }else if(i == 5){
+    digitalWrite(0, LOW);
+    
+    digitalWrite(1, LOW);
     digitalWrite(2, HIGH);
     digitalWrite(3, LOW);
     digitalWrite(4, HIGH);
     digitalWrite(5, HIGH);
     digitalWrite(6, LOW);
-
-  }else if(i == 2){
+  
+  }else if(i == 6){
+    digitalWrite(0, LOW);
+    
+    digitalWrite(1, HIGH);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+  
+  }else if(i == 7){
     digitalWrite(0, LOW);
     
     digitalWrite(1, HIGH);
@@ -39,25 +88,25 @@ void displayNumber(int i){
     digitalWrite(4, LOW);
     digitalWrite(5, LOW);
     digitalWrite(6, HIGH);
-
-  }else if(i == 3){
-    digitalWrite(0, LOW);
+  
+  }else if(i == 8){
+    digitalWrite(0, HIGH);
     
     digitalWrite(1, HIGH);
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-
-  }else if(i == 4){
-    digitalWrite(0, LOW);
-    
-    digitalWrite(1, LOW);
     digitalWrite(2, HIGH);
     digitalWrite(3, LOW);
     digitalWrite(4, HIGH);
     digitalWrite(5, HIGH);
+    digitalWrite(6, LOW);
+  
+  }else if(i == 9){
+    digitalWrite(0, HIGH);
+    
+    digitalWrite(1, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
     digitalWrite(6, LOW);
   }
 }
