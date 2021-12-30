2b1d52c26a3759bccb78262b44d54c4e7a630737
grassnhi <nhi.nguyenn@hcmut.edu.vn>
2021-12-30 19:12:10 +0700

    Add my exercise

---
+++ CC14_Ex/Arduino/MotorMonitor/Exercise/404.ino
@@ -0,0 +1,101 @@
+/*
+Robot Headlights
+
+When a character W, S, A and D is sent, the robot moves forward, backward, left and right,
+respectively. With all other characters, the robot stops moving.
+
+Add two more LEDs to the system and turn an appropriate LED when Robot is turned left
+or right.
+*/
+
+void setup(){
+    Serial.begin (115200);
+
+    pinMode(6, OUTPUT);
+  	pinMode(7, OUTPUT);
+  	pinMode(8, OUTPUT);
+  	pinMode(9, OUTPUT);
+  	pinMode(10, OUTPUT); 
+  	pinMode(11, OUTPUT);
+  	pinMode(12, OUTPUT); 
+  	pinMode(13, OUTPUT);
+}
+
+void forward (int speed){
+    digitalWrite(8, HIGH);
+    digitalWrite(13, HIGH);
+    digitalWrite(11, LOW);
+    digitalWrite(12, LOW);
+  
+    digitalWrite(6, LOW);
+    digitalWrite(7, LOW);
+  
+    analogWrite(9, speed);
+    analogWrite(10, speed);
+}
+
+void backward (int speed ) {
+ 	digitalWrite(8, LOW);
+  	digitalWrite(13, LOW);
+  	digitalWrite(11, HIGH);
+  	digitalWrite(12, HIGH);
+  
+  	digitalWrite(6, LOW);
+  	digitalWrite(7, LOW);
+  
+  	analogWrite(9, speed);
+  	analogWrite(10, speed);
+}
+
+void turnleft (int speed){ 
+ 	digitalWrite(8, LOW);
+  	digitalWrite(13, HIGH);
+  	digitalWrite(11, LOW);
+  	digitalWrite(12, LOW);
+  
+  	digitalWrite(6, HIGH);
+  	digitalWrite(7, LOW);
+  
+  	analogWrite(10, speed);
+}
+
+void turnright (int speed) {
+ 	digitalWrite(8, HIGH);
+  	digitalWrite(13, LOW);
+  	digitalWrite(11, LOW);
+  	digitalWrite(12, LOW);
+  
+  	digitalWrite(6, LOW);
+  	digitalWrite(7, HIGH);
+  
+  	analogWrite(9, speed); 
+}
+
+void loop () {
+  	if( Serial. available() ){
+		char temp = Serial. read (); 
+		Serial.print ("I received : "); 
+		Serial.println ( temp);
+
+		if (temp == 'W')
+			forward (100);
+
+		else if (temp == 'S')
+			backward (100);
+  
+    	else if (temp == 'A')
+			turnleft (100);
+  	
+    	else if (temp == 'D')
+			turnright (100);
+  	
+    	else{
+      		digitalWrite(6, LOW);
+      		digitalWrite(7, LOW);
+      		digitalWrite(8, LOW);
+      		digitalWrite(11, LOW);
+      		digitalWrite(12, LOW);
+     		digitalWrite(13, LOW);
+   	    }
+    }
+}
