558cbe9093feaf1ef636c97802725a18152e641f
grassnhi <nhi.nguyenn@hcmut.edu.vn>
2022-01-15 15:17:04 +0700

    Add question about assignment 0

---
+++ BKLearning/CO1027/Assignment/Ass0/studyInPink.h
@@ -0,0 +1,46 @@
+/*
+    * Ho Chi Minh City University of Technology
+    * Faculty of Computer Science and Engineering
+    * Initial code for Assignment 0
+    * Programming Fundamentals Spring 2022
+    * Author: Tran Huy
+    * Date: 06.01.2022
+*/
+
+//The library here is concretely set, students are not allowed to include any other libraries.
+#ifndef studyInPink_h
+#define studyInPink_h
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////
+///STUDENT'S ANSWER BEGINS HERE
+///Complete the following functions
+///DO NOT modify any parameters in the functions.
+////////////////////////////////////////////////////////////////////////
+
+int firstMeet(int& EXP1, int& EXP2, const int& E1){
+    //Complete this function to gain point on task 1
+    return -1;
+}
+
+int investigateScene(int& EXP1, int& EXP2, int& HP2, int& M2, const int& E2){
+    //Complete this function to gain point on task 2
+    return -1;
+}
+
+int traceLuggage(int& HP1, int& EXP1, int& M1, const int& E3){
+    //Complete this function to gain point on task 3
+    return -1;
+}
+
+////////////////////////////////////////////////
+///END OF STUDENT'S ANSWER
+////////////////////////////////////////////////
+#endif /* studyInPink_h */
