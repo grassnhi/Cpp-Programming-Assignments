793946597b335b0ba1464ddf60085eebdc8fb0e7
grassnhi <nhi.nguyenn@hcmut.edu.vn>
2022-01-21 23:01:56 +0700

    update...

--- BKLearning/CO1027/Assignment/Ass0/Minor/task3.cpp
+++ BKLearning/CO1027/Assignment/Ass0/Minor/task3.cpp
@@ -6,7 +6,17 @@
 using namespace std;
 
 int traceLuggage(int& HP1, int& EXP1, int& M1, const int& E3){
-
+    if( E3 < 0 || E3 > 999)
+            return -999;
+        else{ 
+            int P1[9]= {1,3,5,7,9,11,13,15,17};
+            for(int i=0; i<9; i++){
+                P1[i] = (P1[i]+E3)%26 + 65;
+                if (P1[i]==80){
+                    
+                }
+            }
+    }
 }
 
 int main(){
