574545f19cad022a12fe49dcb8551d650098b43e
grassnhi <nhi.nguyenn@hcmut.edu.vn>
2022-03-27 10:28:09 +0700

    Update my work

--- Self-study/Arduino/Motor/runcar.ino
+++ Self-study/Arduino/Motor/runcar.ino
@@ -6,12 +6,43 @@
 #define MAX_SPEED 255 
 #define MIN_SPEED 0
 
+const int trig = 8;
+const int echo =9;
+
 void setup()
 {
+  Serial.begin(9600);
+
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+}
+
+float sensor(){
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trig, HIGH); 
+  delayMicroseconds(5);
+
+  digitalWrite(trig, LOW);
+
+  float duration = pulseIn(echo, HIGH);
+  return duration/2/29.412; 
+}
+
+void motorRightStop(){
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+}
+ 
+void motorLeftStop(){
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
 
 void rightHead(int speed){
@@ -40,6 +71,17 @@ void leftBack(int speed){
 
 void loop()
 {
- rightHead(200);
- leftHead(100);
+  float distance = sensor();
+  Serial.println(distance);
+  if(distance > 30){
+      rightHead(MAX_SPEED);
+      leftHead(MAX_SPEED);
+  }else{
+      rightHead(MAX_SPEED);
+      leftBack(253);
+      if(distance < 10){
+        motorRightStop();
+        motorLeftStop();
+        }
+    }
 }
